ff1a1a1259a9b844593332bfdb4e1dabe0b0444b
Dane Austin <dane_austin@fastmail.com.au>
2016-07-14 19:09:10 +0100

    setup.py added, installed with pip, seems to work

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name="pyqtgraph_extensions",
+      version=0.1,
+      description="Extensions for pyqtgraph",
+      long_description="""pyqtgraph_extensions many add-ons for pyqtgraph.""",
+      author='Dane Austin',
+      author_email='dane_austin@fastmail.com.au',
+      url='https://bitbucket.org/draustin/pyqtgraph_extensions',
+      license='BSD',
+      packages=['pyqtgraph_extensions', 'pyqtgraph_extended'],
+      requires=['pyqtgraph'],
+)
