ea7c42b2c401eb7d168e2608bcf4e3a7922e9210
Dane on Dane's laptop <none@none>
2017-04-04 18:01:30 +0100

    time axis item

--- pyqtgraph_extensions/AxisItem.py
+++ pyqtgraph_extensions/AxisItem.py
@@ -1,4 +1,4 @@
-import os,logging
+import os,logging,time
 import pyqtgraph as pg
 from pyqtgraph import QtCore,QtGui
 from functools import partial
@@ -305,4 +305,41 @@ class AxisItem(pg.AxisItem):
     def setExportMode(self, export, opts=None):
         pg.AxisItem.setExportMode(self, export, opts)
         self.updateButtons()
+        
+class TimeAxisItem(AxisItem):
+    def tickStrings(self, values, scale, spacing):
+        """Copied from pyqtgraph examples."""
+        strns = []
+        if len(values)==0:
+            return AxisItem.tickStrings(self,values,scale,spacing)
+        rng = max(values)-min(values)
+        #if rng < 120:
+        #    return pg.AxisItem.tickStrings(self, values, scale, spacing)
+        if rng < 3600*24:
+            string = '%H:%M:%S'
+            label1 = '%b %d -'
+            label2 = ' %b %d, %Y'
+        elif rng >= 3600*24 and rng < 3600*24*30:
+            string = '%d'
+            label1 = '%b - '
+            label2 = '%b, %Y'
+        elif rng >= 3600*24*30 and rng < 3600*24*30*24:
+            string = '%b'
+            label1 = '%Y -'
+            label2 = ' %Y'
+        elif rng >=3600*24*30*24:
+            string = '%Y'
+            label1 = ''
+            label2 = ''
+        for x in values:
+            try:
+                strns.append(time.strftime(string, time.localtime(x)))
+            except ValueError:  ## Windows can't handle dates before 1970
+                strns.append('')
+        try:
+            label = time.strftime(label1, time.localtime(min(values)))+time.strftime(label2, time.localtime(max(values)))
+        except ValueError:
+            label = ''
+        #self.setLabel(text=label)
+        return strns
         
